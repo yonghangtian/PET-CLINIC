953046d95bb51e9d2240fd937311d518245e5a18
yonghangtian <yonghangtian@gmail.com>
2019-02-12 21:13:04 +0800

    Initial Sring Boot Project Pet Clinic.

---
+++ src/test/java/tian/springframework/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package tian.springframework.petclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
