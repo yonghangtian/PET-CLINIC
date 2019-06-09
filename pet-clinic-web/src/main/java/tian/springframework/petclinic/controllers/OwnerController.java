5970f11b33288c860f85b0a47cabc178345311a6
yonghangtian <yonghangtian@gmail.com>
2019-06-09 22:19:19 +0800

    list owners on owner index page. Closes #16.

--- pet-clinic-web/src/main/java/tian/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/tian/springframework/petclinic/controllers/OwnerController.java
@@ -1,7 +1,9 @@
 package tian.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tian.springframework.petclinic.services.OwnerService;
 
 /**
  * @author tianyh
@@ -11,9 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     // so here we means "/owners", "owners/", "owners/index", "owners/index.html"
     @RequestMapping({"","/","/index","/index.html"})
-    public String  listOwners(){
+    public String  listOwners(Model model){
+
+        model.addAttribute("owners",this.ownerService.findAll());
         return "owners/index";
     }
 }
