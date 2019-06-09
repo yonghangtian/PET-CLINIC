c953435f4be6850ce88acb6e10fd41425a18a2fa
yonghangtian <yonghangtian@gmail.com>
2019-06-09 22:32:16 +0800

    list vets on vet page. Closes #15.

--- pet-clinic-web/src/main/java/tian/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/tian/springframework/petclinic/controllers/VetController.java
@@ -1,7 +1,9 @@
 package tian.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tian.springframework.petclinic.services.VetService;
 
 /**
  * @author tianyh
@@ -10,8 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String  listVets(){
+    public String  listVets(Model model){
+
+        model.addAttribute("vets", this.vetService.findAll());
+
         return "vets/index";
     }
 }
