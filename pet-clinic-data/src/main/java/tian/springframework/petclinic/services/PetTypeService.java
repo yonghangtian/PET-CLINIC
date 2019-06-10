626e26faa9dbb22f46f7dabe1f4e806850bc44a2
yonghangtian <yonghangtian@gmail.com>
2019-06-10 20:08:57 +0800

    add PetType map service impl. Closes #29.

---
+++ pet-clinic-data/src/main/java/tian/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package tian.springframework.petclinic.services;
+
+import tian.springframework.petclinic.model.PetType;
+
+/**
+ * @author tianyh
+ * created by tianyh on 6/10/19 8:00 PM
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
