5017db52dce7471ca0e13ac95100f1cb7dea0fc0
yonghangtian <yonghangtian@gmail.com>
2019-07-01 09:44:24 +0800

    implement baseEntity. Closes #35.

--- pet-clinic-data/src/main/java/tian/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/tian/springframework/petclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package tian.springframework.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * @author tianyh
- * @since 6/3/19 10:26 PM
+ * created by tian at 6/3/19 10:26 PM
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
