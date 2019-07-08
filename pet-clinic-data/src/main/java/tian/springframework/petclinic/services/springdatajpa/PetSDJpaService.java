f4693a69694b30a85c4fad5d63fd6a1dac896fcb
yonghangtian <yonghangtian@gmail.com>
2019-07-08 16:34:52 +0800

    create spring JPA repository for pet. Closes #43.

---
+++ pet-clinic-data/src/main/java/tian/springframework/petclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,51 @@
+package tian.springframework.petclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import tian.springframework.petclinic.model.Pet;
+import tian.springframework.petclinic.repositories.PetRepository;
+import tian.springframework.petclinic.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * created by tianyh on 7/8/19 4:30 PM
+ */
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+}
