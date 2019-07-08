a255a937ec60a3c2921a397eb403078ee2440e1d
yonghangtian <yonghangtian@gmail.com>
2019-07-08 13:08:00 +0800

    create visit entity and update visit with JPA entity mappings. Closes #37.

--- pet-clinic-data/src/main/java/tian/springframework/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/tian/springframework/petclinic/model/Visit.java
@@ -1,15 +1,24 @@
 package tian.springframework.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * @author tianyh
  * created by tianyh on 6/10/19 5:57 PM
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
