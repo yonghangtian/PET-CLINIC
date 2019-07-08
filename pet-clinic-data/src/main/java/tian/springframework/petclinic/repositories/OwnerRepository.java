871ab783e2a8f0eb5406beb7e2e95febdce8c00c
yonghangtian <yonghangtian@gmail.com>
2019-07-08 14:20:33 +0800

    create spring JPA repository for owner. Closes #40.

--- pet-clinic-data/src/main/java/tian/springframework/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/tian/springframework/petclinic/repositories/OwnerRepository.java
@@ -7,4 +7,6 @@ import tian.springframework.petclinic.model.Owner;
  * created by tianyh on 7/8/19 1:15 PM
  */
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
---
+++ pet-clinic-data/src/main/java/tian/springframework/petclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,65 @@
+package tian.springframework.petclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import tian.springframework.petclinic.model.Owner;
+import tian.springframework.petclinic.repositories.OwnerRepository;
+import tian.springframework.petclinic.repositories.PetRepository;
+import tian.springframework.petclinic.repositories.PetTypeRepository;
+import tian.springframework.petclinic.services.OwnerService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * created by tianyh on 7/8/19 1:30 PM
+ * SD stands for spring data
+ */
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository,
+                             PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long along) {
+        // trick
+        return ownerRepository.findById(along).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+}
