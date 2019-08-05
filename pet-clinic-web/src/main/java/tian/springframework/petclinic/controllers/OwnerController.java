cd6fe1cad1eba69cc735320ad3f069189f93bc7e
yonghangtian <yonghangtian@gmail.com>
2019-08-05 16:21:14 +0800

    display owner display.

--- pet-clinic-web/src/main/java/tian/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/tian/springframework/petclinic/controllers/OwnerController.java
@@ -2,7 +2,10 @@ package tian.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import tian.springframework.petclinic.services.OwnerService;
 
 /**
@@ -31,5 +34,12 @@ public class OwnerController {
     @RequestMapping({"/find"})
     public String findOwner() {
         return "notImplYet";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
