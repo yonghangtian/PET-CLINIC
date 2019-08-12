eeb5324c5cd4ecc6317c6fe8bef9a739878f12cf
yonghangtian <yonghangtian@gmail.com>
2019-08-12 22:42:29 +0800

    Fix visitControllerTest bugs.Closes #64.

--- pet-clinic-web/src/main/java/tian/springframework/petclinic/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/tian/springframework/petclinic/controllers/VisitController.java
@@ -10,6 +10,8 @@ import tian.springframework.petclinic.services.PetService;
 import tian.springframework.petclinic.services.VisitService;
 
 import javax.validation.Valid;
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
 import java.util.Map;
 
 /**
@@ -27,10 +29,16 @@ public class VisitController {
     }
 
     @InitBinder
-    public void setAllowedFields(WebDataBinder dataBinder) {
+    public void dataBinder(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
-    }
 
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException{
+                setValue(LocalDate.parse(text));
+            }
+        });
+    }
     /**
      * Called before each and every @RequestMapping annotated method.
      * 2 goals:
