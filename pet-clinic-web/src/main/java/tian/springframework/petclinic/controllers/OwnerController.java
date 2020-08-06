2b219fb553b4fe2ab3a4ca2cf344ef9c9879b05d
yonghangtian <yonghangtian@gmail.com>
2020-08-06 13:36:15 +0800

    Fix OwnerController again

--- pet-clinic-web/src/main/java/tian/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/tian/springframework/petclinic/controllers/OwnerController.java
@@ -60,7 +60,7 @@ public class OwnerController {
         } else {
             // multiple owners found
             model.addAttribute("selections", results);
-            return "owners/ownersList";
+            return "owners/ownerList";
         }
     }
 
